fc911eded3886a684e0b0f371e2cce66f532764d
Sneha P <72751097+Sneha-Pulse@users.noreply.github.com>
2024-12-09 06:53:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,93 @@
-"use strict";
+$(document).ready(function () {
+  // Scroll down sticky navbar script start
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+    // Scroll down sticky navbar script end
 
-let section = document.querySelector('section');
-let text = document.querySelector('.text');
-let innerText = document.querySelector('.innerText');
-let loremtext = document.getElementById("loremtext");
+    // Scroll up Button script start
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+    // Scroll up Button script end
 
-window.addEventListener('scroll', function() {
-    let value = window.scrollY;
-    section.style.clipPath = `circle(${value}px at center center)`;
-    text.style.left = 100 - value / 5 + `%`;
-    innerText.style.left = 100 - value / 5 + `%`;
-})
+    // Fade In & Fade Out Elements on Scroll script start
+    $(".fadein").each(function (i) {
+      var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      if (bottom_of_window > bottom_of_element) {
+        $(this).addClass("showme");
+      }
+      if (bottom_of_window < bottom_of_element) {
+        $(this).removeClass("showme");
+      }
+    });
+    // Fade In & Fade Out Elements on Scroll script end
+  });
+
+  // Scroll up Button script start
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+  });
+  // Scroll up Button script end
+
+  // animation Script for display
+  const animationScript = ["Technical Writer", "Deatil-oriented Analyst", "Tech enthusiast"];
+
+  // Typing animation script start
+  new Typed(".typing", {
+    strings: animationScript,
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  new Typed(".typing2", {
+    strings: animationScript,
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+  // Typing animation script End
+
+  // toggle menu/navbar script start
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+  // toggle menu/navbar script end
+
+  // owl carousel script start
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
+  // owl carousel script end
+});
+
+// Get the current year
+const currentYear = new Date().getFullYear();
+// Set the current year in the span with id "year"
+document.getElementById('year').textContent = currentYear;
